c21cb1b8c19eb7566105c623a697f23024dbbf1f
Caio Ramos <61852784+caioramos686@users.noreply.github.com>
2021-09-09 21:22:30 -0300

    Add files via upload

---
+++ IEletronico.java
@@ -0,0 +1,12 @@
+package secao14;
+
+public interface IEletronico {
+
+	public String MARCA = "Geek";//constante em maisculo 
+	
+	public void ligar();
+	
+	public void desligar();
+	
+	
+}
---
+++ Programa33.java
@@ -0,0 +1,14 @@
+package secao14;
+//Interfaces
+public class Programa33 {
+
+	public static void main(String[] args) {
+		Ventilador vent = new Ventilador();
+	
+		vent.desligar();
+		
+		vent.ligar(); 
+		
+	}
+
+}
